7774364fe2f212d19fd31bc55c6b33f8116f2267
John Mulligan <jmulligan@redhat.com>
2018-03-29 15:39:30 -0400

    xxx: more unit tests
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- apps/glusterfs/health_cache_test.go
+++ apps/glusterfs/health_cache_test.go
@@ -10,7 +10,7 @@
 package glusterfs
 
 import (
-	_ "fmt"
+	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -107,4 +107,102 @@ func TestNodeHeathCacheMonitor(t *testing.T) {
 	hc.Stop()
 
 	tests.Assert(t, cc >= (2*6), "expected cc >= (2 * 6), got:", cc)
+}
+
+func TestNodeHeathCacheSomeUnhealthy(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app (I'm being lazy here. An app is not strictly
+	// needed but it is convenient.
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	cc := 0
+	app.xo.MockGlusterdCheck = func(host string) error {
+		var e error
+		if cc & 1 == 1 {
+			e = fmt.Errorf("Bloop %v", cc)
+		}
+		cc++
+		return e
+	}
+	hc := NewNodeHealthCache(app.db, app.executor)
+	tests.Assert(t, hc != nil, "expected hc != nil, got:", hc)
+
+	err = hc.Refresh()
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	nodeUp := hc.Status()
+	tests.Assert(t, len(nodeUp) == 6,
+		"expected len(nodeUp) == 6, got:", len(nodeUp))
+	var up, down int
+	for _, v := range nodeUp {
+		if v {
+			up++
+		} else {
+			down++
+		}
+	}
+	tests.Assert(t, up == 3, "expected len(up) == 3, got:", up)
+	tests.Assert(t, down == 3, "expected len(down) == 3, got:", down)
+}
+
+func TestNodeHeathCacheMultiRefresh(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app (I'm being lazy here. An app is not strictly
+	// needed but it is convenient.
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	err := setupSampleDbWithTopology(app,
+		2,    // clusters
+		3,    // nodes_per_cluster
+		4,    // devices_per_node,
+		6*TB, // disksize)
+	)
+	tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+	cc := 0
+	app.xo.MockGlusterdCheck = func(host string) error {
+		var e error
+		if cc & 1 == 1 {
+			e = fmt.Errorf("Bloop %v", cc)
+		}
+		cc++
+		return e
+	}
+
+	for i := 0; i < 5; i++ {
+		cc = 0
+		hc := NewNodeHealthCache(app.db, app.executor)
+		tests.Assert(t, hc != nil, "expected hc != nil, got:", hc)
+
+		err = hc.Refresh()
+		tests.Assert(t, err == nil, "expected err == nil, got:", err)
+
+		nodeUp := hc.Status()
+		tests.Assert(t, len(nodeUp) == 6,
+			"expected len(nodeUp) == 6, got:", len(nodeUp))
+		var up, down int
+		for _, v := range nodeUp {
+			if v {
+				up++
+			} else {
+				down++
+			}
+		}
+		tests.Assert(t, up == 3, "expected len(up) == 3, got:", up)
+		tests.Assert(t, down == 3, "expected len(down) == 3, got:", down)
+	}
 }
